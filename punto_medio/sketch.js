6ce0e3167a76a5a137ce14ad61bc6c22e7a51773
JaimeMartinez97 <theboy.ofhat@gmail.com>
2019-05-07 19:33:25 -0600

    no se hizo nada

--- punto_medio/sketch.js
+++ punto_medio/sketch.js
@@ -1,7 +1,32 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	circulo(30,30,10)
 }
+//examen
+//dda, bresenham, puntomedio
 
 function draw() {
+	//circulo(30,30,10)
+}
+function circulo(px,py,rad){
+	var p =(5/4)-rad
+	if (p<0) {
+			point(px,py)
+			p=p+(2*px)+1
+		}else {
+			px+=1
+			py-=1
+			p=p+(2*px)+1-(2*py)
+		}
+	// for (var k = 0; px >= py; k++) {
+	// 	if (p<0) {
+	// 		point(px,py)
+	// 		p=p+(2*px)+1
+	// 	}else {
+	// 		px+=1
+	// 		py-=1
+	// 		p=p+(2*px)+1-(2*py)
+	// 	}
+	// }
 
 }
